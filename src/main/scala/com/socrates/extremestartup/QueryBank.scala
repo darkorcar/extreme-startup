ae0c94be8a50c097b1a05600b1e274f2f768f9eb
carlos <carlos.pena@192.com>
2017-06-17 09:50:30 +0100

    Even more questions!

--- src/main/scala/com/socrates/extremestartup/QueryBank.scala
+++ src/main/scala/com/socrates/extremestartup/QueryBank.scala
@@ -1,5 +1,6 @@
 package com.socrates.extremestartup
 
+import scala.annotation.tailrec
 import scala.util.Random
 
 trait QueryBank {
@@ -10,11 +11,19 @@ trait QueryBank {
     BasicQuery(1, "what is the capital of Serbia?", "belgrade"),
     BasicQuery(1, "what is the color of the snow?", "white"),
     UnaryOperationQuery(2, "what is the square root of @1@?", { case (a) => Math.sqrt(a).toInt }),
+    UnaryOperationQuery(2, "what is the @1@^2?", { case (a) => a * a }),
+    UnaryOperationQuery(2, "what is the @1@^3?", { case (a) => a * a * 1 }),
+    UnaryOperationQuery(2, "what is the factorial of @1@?", { case (a) => factorial(a) }),
     BinaryOperationQuery(2, "what is @1@ plus @2@?", { case (a, b) => a + b }),
     BinaryOperationQuery(3, "what is @1@ minus @2@?", { case (a, b) => a - b }),
     BinaryOperationQuery(3, "what is @1@ times @2@?", { case (a, b) => a * b }),
     ListOperationQuery(4, "what is the highest number", { list => list.max }),
-    ListOperationQuery(5, "what is the lowest number", { list => list.min })
+    ListOperationQuery(5, "what is the lowest number", { list => list.min }),
+    ListToStringOperationQuery(5, "what are even numbers", { case list: List[Int] => list.filter(_ % 2 == 0).mkString(",") }),
+    ListToStringOperationQuery(5, "what are odd numbers", { case list: List[Int] => list.filter(_ % 2 == 0).mkString(",") }),
+    ListToStringOperationQuery(7, "what are prime numbers", { case list: List[Int] => list.filter(isPrime).mkString(",") }),
+    UnaryOperationQuery(7, "what is the @1@th fibonnaci number", { case (a) => fib(a) }),
+    ListToStringOperationQuery(5, "what is the JSON representation of: ", { case list: List[Int] => s"[${list.mkString(",")}]" })
   )
 
 
@@ -26,6 +35,29 @@ trait QueryBank {
 
     Random.shuffle(filtered).head
   }
+
+  private def fib( n : Int) : Int = n match {
+    case 0 | 1 => n
+    case _ => fib( n-1 ) + fib( n-2 )
+  }
+
+  private def factorial(n: Int): Int = n match {
+    case 0 => 1
+    case _ => n * factorial(n-1)
+  }
+
+  private def isPrime(number: Int): Boolean = {
+
+    def isDivisibleBy(integer: Int, divisor: Int): Boolean = integer % divisor == 0
+
+    if (number == 1) false
+    else {
+      val domain = (2 to math.sqrt(number).toInt).view
+      val range = domain filter (isDivisibleBy(number, _))
+      range.isEmpty
+    }
+  }
+
 
   private def calculateMaxLevel(round: Int): Int = 0
 }
@@ -68,6 +100,16 @@ case class BinaryOperationQuery(override val difficultyLevel: Int, text: String,
 }
 
 case class ListOperationQuery(override val difficultyLevel: Int, text: String, operation: (List[Int]) => Int) extends Query {
+
+  var numbers = List(Random.nextInt(10), Random.nextInt(10), Random.nextInt(10), Random.nextInt(10))
+
+  override val question: String = s"$text:${numbers.mkString(",")}"
+
+  override val expectedAnswer: String = operation(numbers).toString
+
+}
+
+case class ListToStringOperationQuery(override val difficultyLevel: Int, text: String, operation: (List[Int]) => String) extends Query {
 
   var numbers = List(Random.nextInt(10), Random.nextInt(10), Random.nextInt(10), Random.nextInt(10))
 
