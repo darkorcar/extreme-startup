394053332490bb79f97eae05eb45c281be28a9f0
carlos <carlos.pena@192.com>
2017-06-17 09:00:51 +0100

    Add more questions

--- src/main/scala/com/socrates/extremestartup/QueryBank.scala
+++ src/main/scala/com/socrates/extremestartup/QueryBank.scala
@@ -6,8 +6,15 @@ trait QueryBank {
 
 
   private val questions = List(
-    BasicQuery("what is the capital of Serbia?", "Belgrade"),
-    BinaryOperationQuery("what is the sum of @1@ plus @2@?", { case (a, b) => a + b })
+    BasicQuery(0, "echo", "echo"),
+    BasicQuery(1, "what is the capital of Serbia?", "belgrade"),
+    BasicQuery(1, "what is the color of the snow?", "white"),
+    UnaryOperationQuery(2, "what is the square root of @1@?", { case (a) => Math.sqrt(a).toInt }),
+    BinaryOperationQuery(2, "what is @1@ plus @2@?", { case (a, b) => a + b }),
+    BinaryOperationQuery(3, "what is @1@ minus @2@?", { case (a, b) => a - b }),
+    BinaryOperationQuery(3, "what is @1@ times @2@?", { case (a, b) => a * b }),
+    ListOperationQuery(4, "what is the highest number", { list => list.max }),
+    ListOperationQuery(5, "what is the lowest number", { list => list.min })
   )
 
 
@@ -32,13 +39,23 @@ trait Query {
 }
 
 case class BasicQuery(
+                       override val difficultyLevel: Int,
                        override val question: String,
                        override val expectedAnswer: String
-                     ) extends Query {
-  override def difficultyLevel: Int = 1
+                     ) extends Query
+
+
+case class UnaryOperationQuery(override val difficultyLevel: Int, text: String, operation: (Int) => Int) extends Query {
+
+  val number1 = Random.nextInt(10)
+
+  override val question: String = text
+    .replace("@1@", (number1 * number1).toString)
+  override val expectedAnswer: String = operation(number1).toString
+
 }
 
-case class BinaryOperationQuery(text: String, operation: (Int, Int) => Int) extends Query {
+case class BinaryOperationQuery(override val difficultyLevel: Int, text: String, operation: (Int, Int) => Int) extends Query {
 
   val number1 = Random.nextInt(10)
   val number2 = Random.nextInt(10)
@@ -48,5 +65,14 @@ case class BinaryOperationQuery(text: String, operation: (Int, Int) => Int) exte
     .replace("@2@", number2.toString)
   override val expectedAnswer: String = operation(number1, number2).toString
 
-  override def difficultyLevel: Int = 2
+}
+
+case class ListOperationQuery(override val difficultyLevel: Int, text: String, operation: (List[Int]) => Int) extends Query {
+
+  var numbers = List(Random.nextInt(10), Random.nextInt(10), Random.nextInt(10), Random.nextInt(10))
+
+  override val question: String = s"$text:${numbers.mkString(",")}"
+
+  override val expectedAnswer: String = operation(numbers).toString
+
 }
